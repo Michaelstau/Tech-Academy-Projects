83b9f0b2e85a31ceed999709e251a2de2fa8abcd
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-07-24 14:53:44 -0700

    finished Project1, began Project2

--- JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,10 +2,14 @@ var V = "The \"variable\" "; //assigns a value to a variable
 document.write(V + " is alive! "); //concatinates and writes the result on the page
 var D = V + "is dead. "; //concatinates, and assigns a value to a variable
 window.alert(D); //makes an alert pop up when visiting page
+var some_text = "It worked!"
 
 function button_function() {
-  var some_text = "It worked!"
   document.getElementById("the_button").innerHTML = some_text // changes text on the_button
+}
+
+function change_function() {
+  document.getElementById("the_button").innerHTML = "click!"
 }
 
 var Sent1 = "This is the beginning ot the string", Sent2 = " and this is the end of the string" //assigns 2 variable
---
+++ JavaScript Projects/Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function changeText() { //declair a function
+  var here = "in the office", now = "2:34 pm"; //assign two variables
+  document.getElementById("text").innerHTML = "I want you " + here + " by " + now; //concatenate two variables (and some text), and display them in "text" paragraph.
+}
+var sentence = "I am learning"; //assign a global variable
+
+function addStuff() {
+  sentence += " so much!"; //concatenate " so much!" to sentence variable
+  document.getElementById("concatenate").innerHTML = sentence; //display variable in the "concatenate" paragraph.
+}
