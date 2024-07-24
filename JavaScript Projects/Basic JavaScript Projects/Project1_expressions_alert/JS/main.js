dc3a044b79908f397ac4ab9d9e93d81faa102940
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-07-24 13:29:52 -0700

    Minor practice exercises in Basic JS

---
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var V = "The \"variable\" "; //assigns a value to a variable
+document.write(V + " is alive! "); //concatinates and writes the result on the page
+var D = V + "is dead. "; //concatinates, and assigns a value to a variable
+window.alert(D); //makes an alert pop up when visiting page
+
+function button_function() {
+  var some_text = "It worked!"
+  document.getElementById("the_button").innerHTML = some_text // changes text on the_button
+}
+
+var Sent1 = "This is the beginning ot the string", Sent2 = " and this is the end of the string" //assigns 2 variable
+
+var megaVariable = Sent1 + Sent2 //concatinates two variables into a megaVariable
+document.write(megaVariable) //writes the megaVariable to the page
