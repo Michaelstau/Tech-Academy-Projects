31e89ed237e34358627966843b46b8cf84b98dfd
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-07-25 16:18:43 -0700

    added comments to Project4 main.js

--- JavaScript Projects/Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,23 +1,23 @@
-var worldlyVariable = "present"
+var worldlyVariable = "present"; //assigning a global variable.
 
 function smallTown() {
-  var smallTownVariable = "local"
+  var smallTownVariable = "local"; //assigning a local variable.
 }
 
 function meeting() {
-  console.log(smallTownVariable)
+  console.log(smallTownVariable);
   /* document.getElementById("meeting").innerHTML = "worldlyVariable is " + worldlyVariable + ", smallTownVariable's location is " + smallTownVariable; */
 
-}
+} //defining a function that would have used a variable local to smallTown, and using console.log to to troubleshoot the problem.
 
 function coffeeTime() {
   if (new Date().getHours() < 14) {
-    document.getElementById("Coffee").innerHTML = "No, let's grab an espresso!"
+    document.getElementById("Coffee").innerHTML = "No, let's grab an espresso!";
   }
   else {
-    document.getElementById("Coffee").innerHTML = "Yeah, I'd be up all night if I had one now."
+    document.getElementById("Coffee").innerHTML = "Yeah, I'd be up all night if I had one now.";
   }
-}
+} //defining a function that checks to see if the time is earlier than 2:00pm and asks the user to either get coffee, or not to.
 
 function haunted() {
   var concern = document.getElementById("No")
@@ -28,7 +28,7 @@ function haunted() {
   else {
     haunt.innerHTML = "Relax, it's just the wind."
   }
-}
+} //defining a function that sets variables for the "no" checkbox, and "haunted" paragraph. Then checks to see if the checkbox is checked, and alerts the user to either escape the building or relax depending on an if, else logic statement.
 
 function Time_function() {
   var Time = new Date().getHours();
@@ -43,4 +43,4 @@ function Time_function() {
     Reply = "It is evening time"
   }
   document.getElementById("Time_of_day").innerHTML = Reply
-}
+} //defining a function that sets a Time variable to the current hour, and inserts one of various replys into the Time_of_Day paragraph depending on and if, else if, else logic statement.
