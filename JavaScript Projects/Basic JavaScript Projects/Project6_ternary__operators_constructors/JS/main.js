e53165c0963e0d2ce19bcc068ba0484215cfb9a9
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-07-25 13:03:20 -0700

    Added comments to main.js

--- JavaScript Projects/Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,10 +1,11 @@
 function results() {
   var age = document.getElementById("Age").value;
-  var message = (age < 18) ? "too young to vote":"old enough to vote!";
-  var Height = document.getElementById("Height").value;
-  var canRide = ((Height < 52) ?"You are too short to ride, and ":"You are tall enough to ride, and ");
-  document.getElementById("Result").innerHTML = canRide + message;
+  var message = (age < 18) ? "too young to vote":"old enough to vote!"; //asigning message a value based on a ternary condition.
+  var Height = document.getElementById("Height").value; //grabbing the value for Height from document.
+  var canRide = ((Height < 52) ?"You are too short to ride, and ":"You are tall enough to ride, and "); //asigning canRide a value based on a ternary condition.
+  document.getElementById("Result").innerHTML = canRide + message; //filling in the result paragraph with our results.
 }
+//grabbing age and height values from document, using them each in a ternary operation, and displaying a message to the page based on results.
 
 function coffee() {
   this.size = document.getElementById("Size").value;
@@ -12,6 +13,7 @@ function coffee() {
   this.drink = document.getElementById("Drink").value;
   this.milk = document.getElementById("Milk").value;
 }
+//defining a coffee constructor with 4 key:value pairs.
 
 function nested() {
   var order = new coffee()
@@ -20,3 +22,4 @@ function nested() {
      return "I've got a " + order.size + " " + order.shots + " " + order.drink + " with " + order.milk + " for " + document.getElementById("Name").value + " on the bar!"
   }
 }
+//Using a nested function to construct a coffee object and return a value based on that object to the higher function, which uses it to fill in the "Nested_Function" paragraph.
