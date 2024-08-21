648f56df5918a7165c65e105734ea8c7ca0a13d9
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-08-21 15:28:09 -0700

    Update protected.py

--- Python Projects/protected.py
+++ Python Projects/protected.py
@@ -1,9 +1,12 @@
+""" existance class with a private attribute _life and a protected attribute __meaning.  """
 class Exist:
   def __init__(self):
     self._life = True
     self.__meaning = 42
 
+
   def ponder(self):
+    """ prints ponderings based on the objects values of _life and __meaning """
     if self._life and self.__meaning == 42:
       print(f'Your existance is {self._life}, and strangely drawn to the number {self.__meaning}.')
     elif not self._life:
@@ -12,9 +15,11 @@ class Exist:
       print(f'Your existance is {self._life}, but you find {self.__meaning} kind of meaningless.')
 
   def dies(self):
+    """ changes _life to False for the object """
     self._life = False
 
   def meaningless(self, meaning):
+    """ sets the objects __meaning to whatever the meaning property is """
     self.__meaning = meaning
 
 I = Exist()
