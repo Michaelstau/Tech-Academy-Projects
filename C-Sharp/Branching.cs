c986da632bdefef3353a85d5891b5a778ba5e114
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-18 13:14:16 -0700

    Ensured quote displays with 2 decimals

--- C-Sharp/Branching.cs
+++ C-Sharp/Branching.cs
@@ -7,16 +7,12 @@ namespace Branching
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("What is your package weight?");
-
-            //Prompt user to input a weight, if input is greater than 50, display a message and exit program.
             float weight = Convert.ToSingle(Console.ReadLine());
             if (weight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Environment.Exit(0);
             }
-
-            //Prompt user to input 3 dimensional values, if their sum is greater that 50, display a message and exit program.
             Console.WriteLine("What is your package width?");
             float width = Convert.ToSingle(Console.ReadLine());
             Console.WriteLine("What is your package height?");
@@ -28,11 +24,8 @@ namespace Branching
                 Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
                 Environment.Exit(0);
             }
-
-            //multiply the three dimensions (to get volume) by the weight and divide by 100 and round up to 2 decimals.
-            //display this number with a message as the quote to ship a package.
             double quote = Math.Round((double)length * (double)width * (double)height * (double)weight / (double)100, 2);
-            Console.WriteLine("Your package will cost $" + quote + " to ship.");
+            Console.WriteLine("Your package will cost $" + quote.ToString("F") + " to ship.");
         }
     }
 }
