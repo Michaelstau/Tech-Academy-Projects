254ceaee629f6a7133bf2ee6a3ec8b9be891ea58
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-21 10:48:16 -0700

    Create Strings.cs

---
+++ C-Sharp/Strings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "The quick";
+            string string2 = "brown fox";
+            string string3 = "jumped over";
+
+            string concat = string1 + string2 + string3;
+            concat = concat.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string1);
+            sb.Append(string2);
+            sb.Append(string3);
+
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine(concat);
+            Console.ReadLine();
+
+             
+        }
+    }
+}
