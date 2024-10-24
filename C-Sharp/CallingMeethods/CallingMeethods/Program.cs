7817a0a9a5b519a9de3a2c2347ba1a2fa80f60b7
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-24 11:02:17 -0700

    Uploaded callingMethods project

---
+++ C-Sharp/CallingMeethods/CallingMeethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using CallingMeethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number to do math on.");
+            try
+            {   //Getting a number from the user and calling methods from mathIt on that number.
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(number + " squared is " + mathIt.squareIt(number));
+                Console.WriteLine(number + " doubled is " + mathIt.doubleIt(number));
+                Console.WriteLine(number + " times 0 is " + mathIt.zeroIt(number));
+                Console.ReadLine();
+            }
+            catch (FormatException) //Catching format exceptions if user inputs strings or floats.
+            {
+                Console.WriteLine("PLease only enter whole numbers");
+                Console.ReadLine();
+            } 
+            catch (Exception ex) //catching unforseen exceptions.
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ C-Sharp/CallingMeethods/CallingMeethods/mathIt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMeethods
+{
+    public class mathIt
+    {
+        //three simple methods taking an int and returning it squares, doubled, or zeroed out.
+        public static int squareIt(int num)
+        {
+            int solution = num * num;
+            return solution;
+        }
+        public static int doubleIt(int num) 
+        {
+            int solution = num + num;
+            return solution;
+        }
+        public static int zeroIt(int num) 
+        {
+            int solution = num * 0;
+            return solution;
+        }
+    }
+}
