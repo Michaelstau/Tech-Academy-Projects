50af08b314d8683d4f59aff80ca7111637978a61
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-24 14:04:23 -0700

    Uploaded Method project

---
+++ C-Sharp/Method/Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mathy.Add(4));
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number, or nothing.");
+            string read = Console.ReadLine();
+            try
+            {
+                int num2 = Convert.ToInt32(read);
+                Console.WriteLine(Mathy.Add(num1, num2));
+            }
+            catch (FormatException)
+            {
+                if (read == "")
+                {
+                    Console.WriteLine(Mathy.Add(num1));
+                }
+                else
+                {
+                    Console.WriteLine("Only enter a number, or nothing at all please.");
+                }
+            }
+        }
+    }
+}
