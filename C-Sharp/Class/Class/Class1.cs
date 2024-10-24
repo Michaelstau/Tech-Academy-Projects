ab2878e8d03642d98b9fabe91a4dbe56afca12e3
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-24 15:26:14 -0700

    uploaded Class project

---
+++ C-Sharp/Class/Class/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public static class Class1
+    {
+        static public void Method(int num, out int i)
+            //take int num and int variable, and output result of num/2 to the variable.
+        {
+            i = num/2;
+        }
+
+        static public void Method(int num, out int i, int num2)
+            //overload version of above with num/num2 instead of num/2
+        {
+            i = num / num2;
+        }
+    }
+}
---
+++ C-Sharp/Class/Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args) 
+            //initialize result for our class method to output to, ask user for input number (and convert to int),
+            //call Class1.Method using input number, and print output result
+            //get 2nd number from user to use with first number in overloaded Class1.Method and print output result
+        {
+            int result = 0;
+            Console.WriteLine("Input a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Class1.Method(num, out result);
+            Console.WriteLine(result);
+            Console.WriteLine("Input another number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Class1.Method(num, out result, num2);
+            Console.WriteLine(result);
+        }
+    }
+}
