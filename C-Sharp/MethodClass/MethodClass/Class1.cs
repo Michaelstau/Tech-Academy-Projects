f40227e4ee2a6ee5024b00663e857535589fbd88
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-24 14:32:27 -0700

    uploaded MethodClass project

---
+++ C-Sharp/MethodClass/MethodClass/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    internal class Class1
+    {
+        //method that takes 2 parameters, adds 3 to the 1st, and prints the 2nd
+        public static void method(int num1, int num2)
+        {
+            num1 = num1 + 3;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ C-Sharp/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling the method in Class1 with t parameters, then doing it again but specifying parameters by name.
+            Class1.method(5, 7);
+            Class1.method(num1: 7, num2: 18);
+            Console.Read();
+        }
+    }
+}
