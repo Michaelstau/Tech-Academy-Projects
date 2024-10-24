52a93c2abd62a261f51029913a936c1a48f87ab2
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-24 13:18:59 -0700

    Uploaded mainMethod project

---
+++ C-Sharp/mainMethod/mainMethod/Overload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    class Overload
+    {
+        //method that takes int, adds 1 to it and returns it.
+        public static int Overloaded(int num) 
+        {
+            int result = ++num;
+            return result;
+        }
+
+        //method that takes float, doubles it, turns it into an int and returns it.
+        public static int Overloaded(float num)
+        {
+            float result = num + num;
+            return Convert.ToInt32(result);
+        }
+
+        //method that takes a string, converts it to an int if possible, squares it, and returns it.
+        public static int Overloaded(string num)
+        {
+                int result = Convert.ToInt32(num);
+                result = result * result;
+                return result;
+        }
+    }
+}
---
+++ C-Sharp/mainMethod/mainMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling three methods of overload, and printing results.
+            Console.WriteLine(Overload.Overloaded(2));
+            Console.WriteLine(Overload.Overloaded((float)2.64));
+            Console.WriteLine(Overload.Overloaded("two"));
+        }
+    }
+}
