fd39f9a4ba6fa156898eed87049394c3437be518
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-28 12:42:16 -0700

    Created MethodsAndObjects project

---
+++ C-Sharp/MethodsAndObjects/inheritance/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Employee : Person
+    // inherits from Person class, and has an additional ID property.
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ C-Sharp/MethodsAndObjects/inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // instantiates an employee object, and calls the SayName method to print the employee's name to console. 
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", ID = 01};
+            employee.SayName();
+            Console.WriteLine();
+        }
+    }
+}
