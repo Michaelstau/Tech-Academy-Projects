5fef2396ae84dfb0d2a0a9c5727264c8f6dc2170
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-29 13:50:20 -0700

    Completed Parsing Enums project

---
+++ C-Sharp/Parsing Enums/Parsing Enums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what day of the week is it?");
+            //getting user input string, parsing it to Day type and saving it as a variable. Then writing a reply to user.
+            try
+            {
+                
+                Day now = (Day)Enum.Parse(typeof(Day), Console.ReadLine().ToLower());
+                Console.WriteLine($"Thanks for letting me know it's {now}!");
+            }
+            //writing user a message if they've input something other than a day of the week.
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            //pausing so user can read message.
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        // making an enum type Day.
+        public enum Day
+        {
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+        }
+    }
+}
