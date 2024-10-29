b2530a7971b73889fdd695b74532ebef308d767e
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-28 18:25:03 -0700

    Created Parameter project

---
+++ C-Sharp/Parameter/Parameter/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    public class Employee<T> : Person
+
+    // inherits from Person class
+    // adds an Id property, and generic Things property
+    // Implements the SayName method to print a string to the console
+    {
+
+        public List<T> Things { get; set; }
+        public int Id { get; set; }
+        public bool CurrentlyEmployed { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+
+
+    }
+
+}
---
+++ C-Sharp/Parameter/Parameter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Creates two Employee objects with different Things types
+        //Prints all things to console
+
+        {
+
+            Employee<string> employee1 = new Employee<string>()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                CurrentlyEmployed = true,
+                Id = 01,
+                Things = new List<string>() { "One", "Two", "Three" }
+            };
+
+            Employee<int> employee2 = new Employee<int>()
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                CurrentlyEmployed = true,
+                Id = 01,
+                Things = new List<int>() { 1, 2, 3 }
+            };
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Employee1, thing{i}: {employee1.Things[i]}, Employee2 thing{i}: {employee2.Things[i]}\n");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
