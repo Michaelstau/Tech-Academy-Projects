477e7becfb14a7de777802377ec3afe73051a9a5
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-30 15:23:29 -0700

    added Datetime project

---
+++ C-Sharp/DateTime/DateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Create a datetime variable and set it to the current time using datetime, and print it to user.
+            //ask user for a number
+            //add that number of hours to our datetime variable, and print it to the user.
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Hello, it is currently {0}. How many hours would you like to travel forward in time?", now.ToString());
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime future = DateTime.Now.AddHours(hours); 
+            Console.WriteLine("Hello, it is currently {0}.", future);
+
+        }
+    }
+}
