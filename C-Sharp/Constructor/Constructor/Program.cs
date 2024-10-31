f7fae66c4e43d05c36010db647cd039999d3ed9f
Michaelstau <119985374+Michaelstau@users.noreply.github.com>
2024-10-31 12:43:22 -0700

    finished constructor assignment

---
+++ C-Sharp/Constructor/Constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string shop = "Coffeeopolis";
+            Console.WriteLine("Welcome to {0} What kind of coffee would you like?", shop);
+            var coffee = Console.ReadLine();
+            Console.WriteLine("What size?");
+            var size = Console.ReadLine();
+            if (size.ToLower() == "large" || size.ToLower() == "medium" || size.ToLower() == "small")
+            
+            {
+                Order drink = new Order(coffee, size);
+                Console.WriteLine(drink.Drink);
+                Console.ReadLine();
+            }
+            else
+            {
+                Order drink = new Order(coffee);
+                Console.WriteLine(drink.Drink);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ C-Sharp/Constructor/Constructor/order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Order
+    {
+        public Order(string coffee) : this (coffee, "Medium" )
+        { }
+        public Order(string coffee, string size)
+        { 
+            Coffee = coffee;
+            Size = size;
+            Drink = ($"Your {Size}, {Coffee} has been constructed, and is on the bar.");
+        }
+        public string Coffee {  get; set; }
+        public string Size { get; set; }
+        public string Drink { get; set; }
+    }
+}
